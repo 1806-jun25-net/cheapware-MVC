056cb1e2a227e2f4dfe7616313d2989baa29c703
kevinhko <ko.kevin96@gmail.com>
2018-07-26 12:00:22 -0400

    added models in the MVC

---
+++ CheapWare.MVC/CheapWare.WebApp/Models/CPU.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cheapware.WebApp.Models
+{
+    public partial class Cpu
+    {
+        public int Cpuid { get; set; }
+        public string Name { get; set; }
+        public string Speed { get; set; }
+        public int Cores { get; set; }
+        public decimal Price { get; set; }
+        public string Img { get; set; }
+    }
+}
