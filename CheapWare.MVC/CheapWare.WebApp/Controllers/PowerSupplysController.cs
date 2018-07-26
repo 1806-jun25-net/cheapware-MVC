8ef3e0156fb7a44948f8e6ca472bbb7b6ffb79d2
kevinhko <ko.kevin96@gmail.com>
2018-07-26 19:51:11 -0400

    added all the controllers

---
+++ CheapWare.MVC/CheapWare.WebApp/Controllers/PowerSupplysController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Cheapware.WebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CheapWare.WebApp.Controllers
+{
+    public class PowerSupplysController : Controller
+    {
+
+        private readonly static string ServiceUri = "http://localhost:49453/api/";
+
+        public HttpClient HttpClient { get; }
+
+        public PowerSupplysController(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        // GET: Inventorys
+        public async Task<ActionResult> Index()
+        {
+            // don't forget to register HttpClient as a singleton service in Startup.cs.
+
+            var uri = ServiceUri + "cheapware";
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            try
+            {
+                var response = await HttpClient.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Error");
+                }
+
+                string jsonString = await response.Content.ReadAsStringAsync();
+
+                List<PowerSupplys> ps = JsonConvert.DeserializeObject<List<PowerSupplys>>(jsonString);
+
+                return View(ps);
+            }
+            catch (HttpRequestException ex)
+            {
+                // logging
+                return View("Error");
+            }
+        }
+
+        // GET: Inventorys/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Inventorys/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventorys/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(PowerSupplys ps)
+        {
+            try
+            {
+                string jsonString = JsonConvert.SerializeObject(ps);
+
+                var uri = ServiceUri + "cheapware";
+                var request = new HttpRequestMessage(HttpMethod.Post, uri)
+                {
+                    // we set what the Content-Type header will be here
+                    Content = new StringContent(jsonString, Encoding.UTF8, "application/json")
+                };
+
+                var response = await HttpClient.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Error");
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inventorys/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Inventorys/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inventorys/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Inventorys/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
