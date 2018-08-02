725e964e2a68e79d7af2e78c83f5a45a9a032784
kevinhko <ko.kevin96@gmail.com>
2018-08-01 20:08:30 -0400

    added carts

---
+++ CheapWare.MVC/CheapWare.WebApp/Controllers/CartController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Cheapware.WebApp.Models;
+using CheapWare.WebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TodoMvc.Controllers;
+
+namespace CheapWare.WebApp.Controllers
+{
+    public class CartController : AServiceController
+    {
+
+        public CartController(HttpClient httpClient) : base(httpClient)
+        { }
+
+        // GET: Inventorys
+        public async Task<ActionResult> Index()
+        {
+            // don't forget to register HttpClient as a singleton service in Startup.cs.
+
+            var request = CreateRequestToService(HttpMethod.Get, "api/cart");
+
+            try
+            {
+                var response = await HttpClient.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Error");
+                }
+
+                string jsonString = await response.Content.ReadAsStringAsync();
+
+                List<Cart> cart = JsonConvert.DeserializeObject<List<Cart>>(jsonString);
+
+                return View(cart);
+            }
+            catch (HttpRequestException)
+            {
+                // logging
+                return View("Error");
+            }
+        }
+
+        // GET: Inventorys/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Inventorys/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventorys/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Cart cart)
+        {
+            try
+            {
+                string jsonString = JsonConvert.SerializeObject(cart);
+
+                var request = new HttpRequestMessage(HttpMethod.Post, "api/cart");
+                {
+                    // we set what the Content-Type header will be here
+                    request.Content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                };
+
+                var response = await HttpClient.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return View("Error");
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inventorys/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Inventorys/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inventorys/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Inventorys/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CheapWare.MVC/CheapWare.WebApp/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheapWare.WebApp.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public string ProductId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
