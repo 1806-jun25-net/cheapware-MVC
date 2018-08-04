d164dbedc012213a638ea7bc3b048e6597649852
unknown <KyleTTownsend@gmail.com>
2018-08-04 14:33:33 -0400

    started cart view

--- CheapWare.MVC/CheapWare.WebApp/Controllers/CartController.cs
+++ CheapWare.MVC/CheapWare.WebApp/Controllers/CartController.cs
@@ -23,120 +23,17 @@ namespace CheapWare.WebApp.Controllers
         //Get (Index), Add (create), Delete (delete)
 
         // GET: Inventorys
-        public async Task<ActionResult> Index()
-        {
-            // don't forget to register HttpClient as a singleton service in Startup.cs.
 
-            var request = CreateRequestToService(HttpMethod.Get, "api/cart");
 
-            try
-            {
-                var response = await HttpClient.SendAsync(request);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    return View("Error");
-                }
-
-                string jsonString = await response.Content.ReadAsStringAsync();
-
-                List<Cart> cart = JsonConvert.DeserializeObject<List<Cart>>(jsonString);
-
-                return View(cart);
-            }
-            catch (HttpRequestException)
-            {
-                // logging
-                return View("Error");
-            }
-        }
-
-        // GET: Inventorys/Details/5
+        [HttpDelete]
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Inventorys/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: Inventorys/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(Cart cart)
-        {
-            try
-            {
-                string jsonString = JsonConvert.SerializeObject(cart);
 
-                var request = new HttpRequestMessage(HttpMethod.Post, "api/cart");
-                {
-                    // we set what the Content-Type header will be here
-                    request.Content = new StringContent(jsonString, Encoding.UTF8, "application/json");
-                };
 
-                var response = await HttpClient.SendAsync(request);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    return View("Error");
-                }
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Inventorys/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Inventorys/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Inventorys/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Inventorys/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
+        
 }
--- CheapWare.MVC/CheapWare.WebApp/Controllers/CpusController.cs
+++ CheapWare.MVC/CheapWare.WebApp/Controllers/CpusController.cs
@@ -17,37 +17,6 @@ namespace CheapWare.WebApp.Controllers
 
         public CpusController(HttpClient httpClient) : base(httpClient)
         { }
-
-        // GET: Inventorys
-        public async Task<ActionResult> Index()
-        {
-            // don't forget to register HttpClient as a singleton service in Startup.cs.
-
-            var request = CreateRequestToService(HttpMethod.Get, "api/cpus");
-
-            try
-            {
-                var response = await HttpClient.SendAsync(request);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    return View("Error");
-                }
-
-                string jsonString = await response.Content.ReadAsStringAsync();
-
-                List<Cpus> cpus = JsonConvert.DeserializeObject<List<Cpus>>(jsonString);
-
-                return View(cpus);
-            }
-            catch (HttpRequestException)
-            {
-                // logging
-                return View("Error");
-            }
-        }
-
-        // GET: Inventorys/Details/5
         public async Task<ActionResult> Details(string name)
         {
             var request = CreateRequestToService(HttpMethod.Get, "api/cpus/" + name);
@@ -70,88 +39,6 @@ namespace CheapWare.WebApp.Controllers
             catch (HttpRequestException)
             {
                 return View("Error");
-            }
-        }
-
-        // GET: Inventorys/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Inventorys/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(Cpus cpu)
-        {
-            try
-            {
-                string jsonString = JsonConvert.SerializeObject(cpu);
-
-                var request = new HttpRequestMessage(HttpMethod.Post, "api/Cpus");
-                {
-                    // we set what the Content-Type header will be here
-                    request.Content = new StringContent(jsonString, Encoding.UTF8, "application/json");
-                };
-
-                var response = await HttpClient.SendAsync(request);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    return View("Error");
-                }
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Inventorys/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Inventorys/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Inventorys/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Inventorys/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
             }
         }
     }
